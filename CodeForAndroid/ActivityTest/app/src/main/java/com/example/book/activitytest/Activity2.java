906544587ff9c7b1db734f4a3e0ea5ca52028418
xz2dai <40030489+xz2dai@users.noreply.github.com>
2019-07-26 08:56:33 +0800

    update

--- CodeForAndroid/ActivityTest/app/src/main/java/com/example/book/activitytest/Activity2.java
+++ CodeForAndroid/ActivityTest/app/src/main/java/com/example/book/activitytest/Activity2.java
@@ -1,7 +1,12 @@
 package com.example.book.activitytest;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity2 extends AppCompatActivity {
 
@@ -9,5 +14,37 @@ public class Activity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        Button SwitchButton1 = findViewById(R.id.SwitchButton1);
+        Button SwitchButton2 = findViewById(R.id.SwitchButton2);
+        Button SwitchButton3 = findViewById(R.id.SwitchButton3);
+
+        SwitchButton1.setOnClickListener(l);
+        SwitchButton2.setOnClickListener(l);
+        SwitchButton3.setOnClickListener(l);
     }
+
+    View.OnClickListener l = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            Fragment f = null;
+            switch (v.getId()){
+                case R.id.SwitchButton1:
+                    f = new FragmentTest1();
+                    break;
+                case R.id.SwitchButton2:
+                    f = new FragmentTest2();
+                    break;
+                case R.id.SwitchButton3:
+                    f = new FragmentTest2();
+                    break;
+                default:
+                        break;
+            }
+            ft.replace(R.id.frg,f);
+            ft.commit();
+        }
+    };
 }
