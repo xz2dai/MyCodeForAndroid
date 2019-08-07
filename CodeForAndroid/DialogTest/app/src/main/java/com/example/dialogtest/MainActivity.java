797e872d82766dc592de9de43edf75ebdeda5181
xz2dai <40030489+xz2dai@users.noreply.github.com>
2019-08-07 22:32:30 +0800

    新建工程

--- CodeForAndroid/DialogTest/app/src/main/java/com/example/dialogtest/MainActivity.java
+++ CodeForAndroid/DialogTest/app/src/main/java/com/example/dialogtest/MainActivity.java
@@ -3,20 +3,59 @@ package com.example.dialogtest;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.icu.text.IDNA;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button ShowButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        ShowButton = findViewById(R.id.ShowButton1);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button showButton = findViewById(R.id.ShowButton1);
+        showButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowDialog();
+            }
+        });
     }
     public void ShowDialog(){
-        AlertDialog dialog = null;
+        //AlertDialog dialog = null;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("确认对话框");
+        builder.setMessage("Welcome!What's your name?");
+        final View filename = new EditText(MainActivity.this);
+        builder.setView(filename);
+        builder.setPositiveButton("我叫....", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"Hi!欢迎加入我们",Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("不告诉你", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(MainActivity.this,"那好吧...",Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setCancelable(false);
+        AlertDialog dialog = builder.create();
+        dialog.show();
 
     }
+
 }
