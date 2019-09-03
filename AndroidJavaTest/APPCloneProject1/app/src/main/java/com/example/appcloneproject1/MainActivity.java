54bd5c86fe1bb920a4342130a16cab759ce47bed
xz2dai <40030489+xz2dai@users.noreply.github.com>
2019-09-03 22:59:12 +0800

    fragment的动态切换还没写

--- AndroidJavaTest/APPCloneProject1/app/src/main/java/com/example/appcloneproject1/MainActivity.java
+++ AndroidJavaTest/APPCloneProject1/app/src/main/java/com/example/appcloneproject1/MainActivity.java
@@ -1,11 +1,19 @@
 package com.example.appcloneproject1;
 
 import android.drm.DrmStore;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,4 +24,35 @@ public class MainActivity extends AppCompatActivity {
             actionBar.hide();
         }
     }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.title_backButton:
+                finish();
+                break;
+            case R.id.title_menuButton:
+                break;
+            case R.id.left_button:
+                break;
+            case R.id.mid_button:
+                break;
+            case R.id.right_button:
+                break;
+            case R.id.searchLine_Button:
+                Toast.makeText(MainActivity.this,"开发中......",Toast.LENGTH_SHORT);
+            default:
+                break;
+        }
+    }
+
+    private void InitApp(){
+
+    }
+
+    private void LeftButtonClick(){
+        Button button = findViewById(R.id.left_button);
+        button.setBackgroundColor(Color.parseColor("#4595FC"));
+    }
 }
+
