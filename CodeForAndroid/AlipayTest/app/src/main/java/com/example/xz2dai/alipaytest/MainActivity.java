72765d235e73fb0cbb1ad76c0c34712935d5d2cf
xz2dai <40030489+xz2dai@users.noreply.github.com>
2019-10-09 22:20:50 +0800

    update

--- CodeForAndroid/AlipayTest/app/src/main/java/com/example/xz2dai/alipaytest/MainActivity.java
+++ CodeForAndroid/AlipayTest/app/src/main/java/com/example/xz2dai/alipaytest/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.xz2dai.alipaytest;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
@@ -12,122 +11,34 @@ import android.os.Message;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
-import com.alipay.sdk.app.*;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.alipay.sdk.app.*;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static final String APPID = "2016101300678068";
-    public final String RSA2_PRIVATE = getString(R.string.RSA2_PRIVATE);
-    public final String ALIPAY_PUBLIC_KEY = getString(R.string.ALIPAY_PUBLIC_KEY);
-    public static final String RSA_PRIVATE = "";
-    private static final int SDK_PAY_FLAG = 1;
-    private static final int SDK_AUTH_FLAG = 2;
+    Button paybutton;
+    EditText editText;
+    AliPay aliPay;
 
-    @SuppressLint("HandlerLeak")
-    private Handler mHandler = new Handler() {
-        @SuppressWarnings("unused")
-        public void handleMessage(Message msg) {
-            switch (msg.what) {
-                case SDK_PAY_FLAG: {
-                    @SuppressWarnings("unchecked")
-                    PayResult payResult = new PayResult((Map<String, String>) msg.obj);
-                    /*
-                     * 对于支付结果，请商户依赖服务端的异步通知结果。同步通知结果，仅作为支付结束的通知。
-                     */
-                    String resultInfo = payResult.getResult();// 同步返回需要验证的信息
-                    String resultStatus = payResult.getResultStatus();
-                    // 判断resultStatus 为9000则代表支付成功
-                    if (TextUtils.equals(resultStatus, "9000")) {
-                        // 该笔订单是否真实支付成功，需要依赖服务端的异步通知。
-                        showAlert(MainActivity.this, getString(R.string.pay_success) + payResult);
-                    } else {
-                        // 该笔订单真实的支付结果，需要依赖服务端的异步通知。
-                        showAlert(MainActivity.this, getString(R.string.pay_failed) + payResult);
-                    }
-                    break;
-                }
-                case SDK_AUTH_FLAG: {
-                    @SuppressWarnings("unchecked")
-                    AuthResult authResult = new AuthResult((Map<String, String>) msg.obj, true);
-                    String resultStatus = authResult.getResultStatus();
-
-                    // 判断resultStatus 为“9000”且result_code
-                    // 为“200”则代表授权成功，具体状态码代表含义可参考授权接口文档
-                    if (TextUtils.equals(resultStatus, "9000") && TextUtils.equals(authResult.getResultCode(), "200")) {
-                        // 获取alipay_open_id，调支付时作为参数extern_token 的value
-                        // 传入，则支付账户为该授权账户
-                        showAlert(MainActivity.this, getString(R.string.auth_success) + authResult);
-                    } else {
-                        // 其他状态值则为授权失败
-                        showAlert(MainActivity.this, getString(R.string.auth_failed) + authResult);
-                    }
-                    break;
-                }
-                default:
-                    break;
-            }
-        }
-    };
-
-    public void payV2(View v) {
-        if (TextUtils.isEmpty(APPID) || (TextUtils.isEmpty(RSA2_PRIVATE))) {
-            showAlert(this, getString(R.string.error_missing_appid_rsa_private));
-            return;
-        }
-
-        /*
-         * 这里只是为了方便直接向商户展示支付宝的整个支付流程；所以Demo中加签过程直接放在客户端完成；
-         * 真实App里，privateKey等数据严禁放在客户端，加签过程务必要放在服务端完成；
-         * 防止商户私密数据泄露，造成不必要的资金损失，及面临各种安全风险；
-         *
-         * orderInfo 的获取必须来自服务端；
-         */
-        boolean rsa2 = (RSA2_PRIVATE.length() > 0);
-        Map<String, String> params = OrderInfoUtil2_0.buildOrderParamMap(APPID, rsa2);
-        String orderParam = OrderInfoUtil2_0.buildOrderParam(params);
-
-        String privateKey = rsa2 ? RSA2_PRIVATE : RSA_PRIVATE;
-        String sign = OrderInfoUtil2_0.getSign(params, privateKey, rsa2);
-        final String orderInfo = orderParam + "&" + sign;
-
-        final Runnable payRunnable = new Runnable() {
-
-            @Override
-            public void run() {
-                PayTask alipay = new PayTask(MainActivity.this);
-                Map<String, String> result = alipay.payV2(orderInfo, true);
-                Log.i("msp", result.toString());
-
-                Message msg = new Message();
-                msg.what = SDK_PAY_FLAG;
-                msg.obj = result;
-                mHandler.sendMessage(msg);
-            }
-        };
-
-        // 必须异步调用
-        Thread payThread = new Thread(payRunnable);
-        payThread.start();
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         EnvUtils.setEnv(EnvUtils.EnvEnum.SANDBOX);              //沙箱环境
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        aliPay = new AliPay(MainActivity.this);
+        paybutton = findViewById(R.id.payButton);
+        editText =findViewById(R.id.editText);
+        paybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                aliPay.payV2(editText.getText().toString());
+            }
+        });
     }
 
-    private static void showAlert(Context ctx, String info) {
-        showAlert(ctx, info, null);
-    }
 
-    private static void showAlert(Context ctx, String info, DialogInterface.OnDismissListener onDismiss) {
-        new AlertDialog.Builder(ctx)
-                .setMessage(info)
-                .setPositiveButton(R.string.confirm, null)
-                .setOnDismissListener(onDismiss)
-                .show();
-    }
 }
