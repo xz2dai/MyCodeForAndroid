711c6d365bfc1f0bada429968b4198ead3c3e714
xz2dai <40030489+xz2dai@users.noreply.github.com>
2020-07-03 23:59:49 +0800

    update

--- PicPic/app/src/main/java/com/xz2dai/picpic/MainActivity.kt
+++ PicPic/app/src/main/java/com/xz2dai/picpic/MainActivity.kt
@@ -1,11 +1,63 @@
 package com.xz2dai.picpic
 
-import androidx.appcompat.app.AppCompatActivity
+import PixivDownloader
+import android.graphics.Bitmap
 import android.os.Bundle
+import android.util.DisplayMetrics
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.concurrent.thread
 
-class MainActivity : AppCompatActivity() {
+
+class MainActivity : AppCompatActivity(),View.OnClickListener  {
+
+    val pixivDownloader:PixivDownloader = PixivDownloader()
+    var reSize:Boolean = false
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_getIMG.setOnClickListener(this)
+        btn_changeLocal.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        when(v){
+            btn_getIMG -> getImg()
+            btn_changeLocal -> changeLocal()
+            imageview ->{
+                if (!reSize){
+                    //初始处于缩放状态，点击后放大到全屏
+                }else{
+                    //处于放大状态，缩放图片
+                }
+
+            }
+        }
+    }
+
+    private fun changeLocal() {
+
+    }
+
+    fun getImg(){
+        var pvid:String
+        var bitmap:Bitmap
+        if(edittext.text!=null){
+            pvid = edittext.text.toString()
+            thread {
+                bitmap = pixivDownloader.GetIMG(pvid)       //获取图片
+                runOnUiThread(Runnable {        //修改View需要在创建view的线程中进行
+                    imageview.setImageBitmap(bitmap)
+                })
+            }
+        }else{
+            Toast.makeText(this, "please input a pvid", Toast.LENGTH_SHORT).show()
+        }
+    }
+
 }
--- PicPic/app/src/main/java/com/xz2dai/picpic/PixivDownloader.kt
+++ PicPic/app/src/main/java/com/xz2dai/picpic/PixivDownloader.kt
@@ -5,10 +5,11 @@ import java.io.*
 import java.net.URL
 import java.net.URLConnection
 
-fun main(){
+fun main() {
     val getimg = PixivDownloader()
     getimg.GetIMG("82429481")
 }
+
 /*
 *本app最基本也是最核心的类
 *与pixiv网络交互
@@ -19,29 +20,35 @@ class PixivDownloader {
     /*
     获取制定pvid的图片
      */
-        fun GetIMG(pvid:String,name:String? = "test"){
-            val savePath:String = "D:\\image\\"
-            var uri:String = "https://pixiv.cat/$pvid.jpg"
-            val bitmap = downloadURLIMG(uri)
-        }
-        @Throws(Exception::class)
-        fun downloadURLIMG(urlString: String?):Bitmap {
-            // 构造URL
-            val url = URL(urlString)
-            // 打开连接
-            val con: URLConnection = url.openConnection()
-            //设置请求超时为5s
-            con.setConnectTimeout(2 * 1000)
-            // 输入流
-            val `is`: InputStream = con.getInputStream()
-            //从流中获取bitmap
-            val bitmap:Bitmap = BitmapFactory.decodeStream(`is`)
-            // 完毕，关闭所有链接
-            `is`.close()
-            return bitmap
-        }
+    fun GetIMG(pvid: String, name: String? = "test"): Bitmap {
+        val savePath: String = "D:\\image\\"
+        var uri: String = "https://pixiv.cat/$pvid.jpg"
+        val bitmap = downloadURLIMG(uri)
+        return bitmap
+    }
 
-    fun SaveBitmapToLocal(bitmap: Bitmap?, filename: String = "test.jpg",savePath: String? = "D:\\image\\") {
+    @Throws(Exception::class)
+    fun downloadURLIMG(urlString: String?): Bitmap {
+        // 构造URL
+        val url = URL(urlString)
+        // 打开连接
+        val con: URLConnection = url.openConnection()
+        //设置请求超时为5s
+        con.setConnectTimeout(2 * 1000)
+        // 输入流
+        val `is`: InputStream = con.getInputStream()
+        //从流中获取bitmap
+        val bitmap: Bitmap = BitmapFactory.decodeStream(`is`)
+        // 完毕，关闭所有链接
+        `is`.close()
+        return bitmap
+    }
+
+    fun SaveBitmapToLocal(
+        bitmap: Bitmap?,
+        filename: String = "test.jpg",
+        savePath: String? = "D:\\image\\"
+    ) {
         if (bitmap == null) {
             return
         }
