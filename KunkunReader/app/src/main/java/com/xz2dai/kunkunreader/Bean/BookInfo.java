10a65c090e36833c63a5117a522cdd8575e2297a
xz2dai <40030489+xz2dai@users.noreply.github.com>
2020-07-21 16:34:10 +0800

    update

--- KunkunReader/app/src/main/java/com/xz2dai/kunkunreader/Bean/BookInfo.java
+++ KunkunReader/app/src/main/java/com/xz2dai/kunkunreader/Bean/BookInfo.java
@@ -1,4 +1,58 @@
 package com.xz2dai.kunkunreader.Bean;
 
 public class BookInfo {
+    private String bookNum; //书名
+    private String Count;   //总字数
+    private String Author;  //作者
+    private String Style;   //分类
+    private String chapterNum;  //章节号
+    private String Content; //内容
+
+    public String getBookNum() {
+        return bookNum;
+    }
+
+    public void setBookNum(String bookNum) {
+        this.bookNum = bookNum;
+    }
+
+    public String getCount() {
+        return Count;
+    }
+
+    public void setCount(String count) {
+        Count = count;
+    }
+
+    public String getAuthor() {
+        return Author;
+    }
+
+    public void setAuthor(String author) {
+        Author = author;
+    }
+
+    public String getStyle() {
+        return Style;
+    }
+
+    public void setStyle(String style) {
+        Style = style;
+    }
+
+    public String getChapterNum() {
+        return chapterNum;
+    }
+
+    public void setChapterNum(String chapterNum) {
+        this.chapterNum = chapterNum;
+    }
+
+    public String getContent() {
+        return Content;
+    }
+
+    public void setContent(String content) {
+        Content = content;
+    }
 }
--- KunkunReader/app/src/main/java/com/xz2dai/kunkunreader/ReadActivity.kt
+++ KunkunReader/app/src/main/java/com/xz2dai/kunkunreader/ReadActivity.kt
@@ -1,30 +1,78 @@
 package com.xz2dai.kunkunreader
 
 import android.annotation.SuppressLint
+import android.icu.text.Transliterator
+import android.opengl.Visibility
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.view.Window
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_read.*
 
 /**
- * An example full-screen activity that shows and hides the system UI (i.e.
- * status bar and navigation/system bar) with user interaction.
+ * 阅读activity，指小说阅读界面，主要显示小说内容，点击屏幕左右两边翻页，点击中间呼出菜单
  */
-class ReadActivity : AppCompatActivity(),View.OnClickListener {
+class ReadActivity : AppCompatActivity(),View.OnClickListener, View.OnTouchListener {
 
+    var MenuIsOpen:Boolean = false
 
-    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_read)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE) //取消标题栏
 
-
+        Read_ContentTextView.setOnClickListener(this)
 
     }
 
     override fun onClick(v: View?) {
+        when(v){
+            Read_back -> this.finish()
+        }
+    }
 
+    override fun onTouch(p0: View?, p1: MotionEvent?): Boolean {
+        var startX:Float = 0f
+        var startY:Float = 0f
+        var endX:Float = 0f
+        var endY:Float = 0f
+        when (p1!!.action) {
+            MotionEvent.ACTION_DOWN ->{
+                startX = p1.x
+                startY = p1.y
+            }
+            MotionEvent.ACTION_MOVE ->{
+
+            }
+            MotionEvent.ACTION_UP -> {
+                endX = p1.x
+                endY = p1.y
+
+                var i = endX - startX
+
+                when(startX){
+                    in 0..p0!!.width/3 -> {
+                        //在左三分之一，向左翻页
+                    }
+                    in p0.width*2/3..p0.width ->{
+                        //在右三分之一，向右翻页
+                    }
+                    else ->{
+                        //其他位置打开菜单
+                        if(MenuIsOpen){
+                            Read_TopBar.visibility = View.INVISIBLE
+                            MenuIsOpen = false
+                        }else{
+                            Read_TopBar.visibility = View.VISIBLE
+                            MenuIsOpen = true
+                        }
+                    }
+
+                }
+            }
+        }
+        return true
     }
 }
